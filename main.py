55b1baba9dbd15d9517283b674f2677047891dc7
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-07-30 15:23:04 +0200

    Add a pipeline skeleton to our project

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from dengai!")
+from sklearn.pipeline import Pipeline
 
+def create_pipeline():
+    return Pipeline([
+        ('preprocessing', None),  # TODO: Add custom preprocessor
+        ('model', None),         # TODO: Add custom model wrapper
+        ('save', None)          # TODO: Add CSV saver
+    ])
+
+def main():
+    print("main() called...")
+
+    # TODO: load CSVs and assign to these variables
+    X_train = None  # TODO: Load training data
+    y_train = None  # TODO: Load training labels
+    X_test = None   # TODO: Load test data
+
+    if any(x is None for x in [X_train, y_train, X_test]):
+        raise NotImplementedError("Data loading not implemented yet")
+
+    pipeline = create_pipeline()
+
+    if any(step[1] is None for step in pipeline.steps):
+        raise NotImplementedError("Pipeline steps not implemented yet")
+
+    pipeline.fit(X_train, y_train)
+    predictions_csv = pipeline.transform(X_test)
+    return predictions_csv
 
 if __name__ == "__main__":
-    main()
+    try:
+        result = main()
+    except NotImplementedError as e:
+        print(f"Setup needed: {e}")
